c664935a00d998cc9f4048aecc0714c5e476e0c6
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2019-01-10 11:27:11 -0700

    Updates

---
+++ precache-manifest.794c42d2a4211d5f2752929032eabb9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe9a43f482cc3b6cdd13",
+    "url": "/test-portfolio/static/js/runtime~main.fe9a43f4.js"
+  },
+  {
+    "revision": "0205329f14270e63af1a",
+    "url": "/test-portfolio/static/js/main.0205329f.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/test-portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0205329f14270e63af1a",
+    "url": "/test-portfolio/static/css/main.e9c46a26.chunk.css"
+  },
+  {
+    "revision": "ae5b218f79c52e00c9a3000de73ebb26",
+    "url": "/test-portfolio/index.html"
+  }
+];
